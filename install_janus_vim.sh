33eb9ae41db89c61f1bea9cb3bd5169939bc4d70
Tonny Xu <tonny.xu@gmail.com>
2013-04-02 01:37:16 +0900

    updated install_janus_vim.sh to make it work.

--- install_janus_vim.sh
+++ install_janus_vim.sh
@@ -1,15 +1,19 @@
 #! /bin/sh
 
-if [ ! -d "#{home}/.janus" ]; then
-  mkdir "#{home}/.janus"
+if [ ! -d "${HOME}/.janus" ]; then
+  mkdir "${HOME}/.janus"
 else
-  rm -rf "#{home}/.janus"
-  mkdir "#{home}/.janus"
+  rm -rf "${HOME}/.janus"
+  mkdir "${HOME}/.janus"
 fi
 
 # we will definitely have .janus ready 
-git clone git@github.com:Lokaltog/powerline.git "#{home}/.janus/powerline"
+git clone git@github.com:Lokaltog/vim-powerline.git "${HOME}/.janus/powerline"
 
 # install janus, will move current .vim to .vim.old
+if [ -d "${HOME}/.vim.old" ]; then
+  rm -rf "${HOME}/.vim.old"
+fi
+
 curl -Lo- https://bit.ly/janus-bootstrap | bash
 
