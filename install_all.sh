5fb491169163678df8989cda2895a8ccb191b2ac
Tonny Xu <tonny.xu@gmail.com>
2017-07-21 16:26:36 +0900

    updated install_all bash to install janus, brew, rbenv, pyenv at first

--- install_all.sh
+++ install_all.sh
@@ -1,3 +1,6 @@
+# run janus vim installation
+./install_janus_vim.sh
+
 cp ./.git-flow-completion.sh $HOME/
 cp ./.git-completion.sh $HOME/
 cp ./.git-prompt.sh $HOME/
@@ -12,3 +15,29 @@ fi
 
 cp ./.gvimrc* $HOME/
 cp ./.vimrc* $HOME/
+
+
+# install brew(xcode is necessary)
+installTool() {
+  if hash $1 2>/dev/null; then
+    echo "Found $1"
+  else
+    echo "Not found $1, $2"
+    exit 1
+  fi
+}
+
+installOrUpdateTool() {
+  if hash $1 2>/dev/null; then
+    echo "Found $1, run $2"
+    $2
+  else
+    echo "    running $3"
+    $3
+  fi
+}
+
+installTool xcode-select "please install Xcode at first"
+installOrUpdateTool brew "brew update" "/usr/bin/ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'"
+installOrUpdateTool rbenv "" "brew install rbenv ruby-install"
+installOrUpdateTool pyenv "" "brew install pyenv"
